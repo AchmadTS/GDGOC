9c5607a71641596321a49ac09eaa17099adeab2a
AchmadTS <achmadtirtosudirosudiro@gmail.com>
2026-03-19 18:33:48 +0700

    feat: Implement CRUD operations for books with validation and routing

---
+++ SG8BE-CRUD_API_and_Swagger_Implementation/src/routes/book.route.ts
@@ -0,0 +1,20 @@
+import { Router } from "express"
+import * as controller from "../controllers/book.controller.js"
+import { validate } from "../middlewares/validation.middleware.js"
+import { bookSchema } from "../schemas/book.schema.js"
+
+const router = Router()
+
+router.get("/", controller.findAll)
+router.get("/:id", controller.findOne)
+
+router.post("/",
+    validate(bookSchema),
+    controller.create)
+router.put("/:id",
+    validate(bookSchema),
+    controller.update)
+router.delete("/:id",
+    controller.remove)
+
+export default router
